a5234e1f5c7305d9509e0a43902cd8228f26215d
redpeacock78 <scarletpeacock78@gmail.com>
2026-09-18 03:21:53 +0900

    feat(parser): Surface の式の構文解析を足す

---
+++ model/redex/parser.rkt
@@ -0,0 +1,286 @@
+#lang racket
+
+(require "lexer.rkt"
+         "diagnostic.rkt")
+
+(provide parse)
+
+;; spec §5.1。lexer の診断は素通りする。呼ぶ側に「lex の結果を場合分けして
+;; から parse を呼ぶ」手続きを課すと、その場合分けを忘れた経路が静かに落ちる。
+(define (parse tokens)
+  (cond
+    [(diagnostic? tokens) tokens]
+    [else
+     (define ts (list->vector tokens))
+     (let/ec return
+       (define (fail key s)
+         (return (diagnostic-of 'surface key #:primary-span s)))
+       (let-values ([(program _) (parse-program ts fail)])
+         program))]))
+
+(define (tok ts i)
+  (vector-ref ts (min i (sub1 (vector-length ts)))))
+(define (kind-at ts i) (stok-kind (tok ts i)))
+(define (value-at ts i) (stok-value (tok ts i)))
+(define (span-at ts i) (stok-span (tok ts i)))
+
+(define (span-id s) (second s))
+(define (span-lo s) (third s))
+(define (span-hi s) (fourth s))
+
+;; spec §5.3。節点の span は最初のトークンの左端から最後のトークンの右端まで
+;; である。区切りの nl は含めない。
+(define (hull a b)
+  `(#:span ,(span-id a) ,(min (span-lo a) (span-lo b))
+           ,(max (span-hi a) (span-hi b))))
+
+;; Surface の節点はすべて (Ctor span ...) の形なので、span は第 2 要素である
+;; （spec §5.2）。宣言と式の両方に使う。
+(define (node-span n) (second n))
+
+(define (skip-nl ts i)
+  (if (eq? (kind-at ts i) 'nl) (add1 i) i))
+
+(define (punct? ts i p)
+  (and (eq? (kind-at ts i) 'punct) (eq? (value-at ts i) p)))
+
+(define (kw? ts i k)
+  (and (eq? (kind-at ts i) 'kw) (eq? (value-at ts i) k)))
+
+(define (eof? ts i) (eq? (kind-at ts i) 'eof))
+
+(define (fail-at ts fail i)
+  (fail (if (eof? ts i) 'surface-unexpected-eof 'surface-unexpected-token)
+        (span-at ts i)))
+
+(define (expect-punct ts fail i p)
+  (if (punct? ts i p)
+      (values (span-at ts i) (add1 i))
+      (fail-at ts fail i)))
+
+(define (expect-ident ts fail i)
+  (if (eq? (kind-at ts i) 'ident)
+      (values (value-at ts i) (span-at ts i) (add1 i))
+      (fail-at ts fail i)))
+
+(define (parse-program ts fail)
+  (define i0 (skip-nl ts 0))
+  (if (eof? ts i0)
+      (fail 'surface-unexpected-eof (span-at ts i0))
+      (let-values ([(expr i1) (parse-expr ts fail i0)])
+        (define i2 (skip-nl ts i1))
+        (if (not (eof? ts i2))
+            (fail-at ts fail i2)
+            (values `(SProgram ,(node-span expr) () ,expr) i2)))))
+
+(define (parse-expr ts fail i)
+  (parse-postfix ts fail i))
+
+(define (parse-postfix ts fail i)
+  (let-values ([(base i0) (parse-primary ts fail i)])
+    (let loop ([node base] [j i0])
+      (cond
+        [(punct? ts j '|(|)
+         (let*-values ([(args j0) (parse-args ts fail (add1 j))]
+                       [(close close-j) (expect-punct ts fail j0 '|)|)])
+           (loop `(SApply ,(hull (node-span node) close) ,node ,args) close-j))]
+        [(punct? ts j '|.|)
+         (define dot-j (add1 j))
+         (let-values ([(name s label-j) (expect-ident ts fail dot-j)])
+           (define label `(SLabel ,s ,name))
+           (loop `(SProj ,(hull (node-span node) s) ,node ,label) label-j))]
+        [else (values node j)]))))
+
+(define (parse-primary ts fail i)
+  (cond
+    [(eq? (kind-at ts i) 'int)
+     (values `(SInt ,(span-at ts i) ,(value-at ts i)) (add1 i))]
+    [(eq? (kind-at ts i) 'str)
+     (values `(SStr ,(span-at ts i) ,(value-at ts i)) (add1 i))]
+    [(kw? ts i 'true)
+     (values `(SBool ,(span-at ts i) true) (add1 i))]
+    [(kw? ts i 'false)
+     (values `(SBool ,(span-at ts i) false) (add1 i))]
+    [(eq? (kind-at ts i) 'ident)
+     (values `(SVar ,(span-at ts i) ,(value-at ts i)) (add1 i))]
+    [(kw? ts i 'fn) (parse-anon-fn ts fail i)]
+    [(punct? ts i '|(|)
+     (define open (span-at ts i))
+     (define j (add1 i))
+     (cond
+       [(punct? ts j '|)|)
+        (values `(SUnit ,(hull open (span-at ts j))) (add1 j))]
+       [else
+        (let*-values ([(expr j0) (parse-expr ts fail j)]
+                      [(close j1) (expect-punct ts fail j0 '|)|)])
+          (values expr j1))])]
+    [(punct? ts i '|{|) (parse-block-or-record ts fail i)]
+    [(eof? ts i) (fail 'surface-unexpected-eof (span-at ts i))]
+    [else (fail-at ts fail i)]))
+
+(define (parse-anon-fn ts fail i)
+  (define start (span-at ts i))
+  (let*-values ([(open open-j) (expect-punct ts fail (add1 i) '|(|)]
+                [(params params-j) (parse-params ts fail open-j)]
+                [(close close-j) (expect-punct ts fail params-j '|)|)])
+    (let*-values ([(return-type type-j) (parse-ty ts fail close-j)]
+                 [(body body-j) (parse-block ts fail type-j)])
+      (values `(SFn ,(hull start (node-span body)) ,params ,return-type ,body)
+              body-j))))
+
+(define (record-ahead? ts i)
+  (define j (skip-nl ts (add1 i)))
+  (or (punct? ts j '|}|)
+      (and (eq? (kind-at ts j) 'ident)
+           (punct? ts (add1 j) '|:|))))
+
+(define (parse-block-or-record ts fail i)
+  (if (record-ahead? ts i)
+      (parse-record ts fail i)
+      (parse-block ts fail i)))
+
+(define (parse-record ts fail i)
+  (define open (span-at ts i))
+  (define j0 (skip-nl ts (add1 i)))
+  (if (punct? ts j0 '|}|)
+      (values `(SRec ,(hull open (span-at ts j0)) ()) (add1 j0))
+      (let loop ([j j0] [fields '()])
+        (let*-values ([(label label-span label-j) (expect-ident ts fail j)]
+                      [(colon colon-j) (expect-punct ts fail label-j '|:|)])
+          (let*-values ([(value value-j) (parse-expr ts fail colon-j)])
+            (define field
+              `(SField ,(hull label-span (node-span value))
+                       (SLabel ,label-span ,label) ,value))
+            (define next (skip-nl ts value-j))
+            (cond
+              [(punct? ts next '|}|)
+               (values `(SRec ,(hull open (span-at ts next))
+                              ,(reverse (cons field fields)))
+                       (add1 next))]
+              [(punct? ts next '|,|)
+               (define after (skip-nl ts (add1 next)))
+               (if (punct? ts after '|}|)
+                   (values `(SRec ,(hull open (span-at ts after))
+                                  ,(reverse (cons field fields)))
+                           (add1 after))
+                   (loop after (cons field fields)))]
+              [(> next value-j)
+               (loop next (cons field fields))]
+              [else (fail-at ts fail next)]))))))
+
+(define (parse-block ts fail i)
+  (define open (span-at ts i))
+  (let loop ([j (skip-nl ts (add1 i))] [bindings '()])
+    (if (or (kw? ts j 'const) (kw? ts j 'let))
+        (let-values ([(binding binding-j) (parse-binding ts fail j)])
+          (if (not (eq? (kind-at ts binding-j) 'nl))
+              (fail-at ts fail binding-j)
+              (loop (skip-nl ts binding-j) (cons binding bindings))))
+        (let*-values ([(body body-j) (parse-expr ts fail j)]
+                     [(close close-j)
+                      (let ([after-body (skip-nl ts body-j)])
+                        (expect-punct ts fail after-body '|}|))])
+          (values `(SBlock ,(hull open close) ,(reverse bindings) ,body)
+                  close-j)))))
+
+(define (parse-bmode ts fail i)
+  (cond
+    [(kw? ts i 'const) (values 'const (add1 i))]
+    [(kw? ts i 'let)
+     (if (kw? ts (add1 i) 'mut)
+         (values 'mut (+ i 2))
+         (values 'let (add1 i)))]
+    [else (fail-at ts fail i)]))
+
+(define (parse-binding ts fail i)
+  (define mode-span (span-at ts i))
+  (let*-values ([(mode mode-j) (parse-bmode ts fail i)]
+               [(name name-span name-j) (expect-ident ts fail mode-j)])
+    (define name-node `(SName ,name-span ,name))
+    (define-values (annotation annotation-j)
+      (if (punct? ts name-j '|:|)
+          (let-values ([(ty ty-j) (parse-ty ts fail (add1 name-j))])
+            (values ty ty-j))
+          (values '#:none name-j)))
+    (let*-values ([(equal equal-j) (expect-punct ts fail annotation-j '|=|)]
+                  [(value value-j) (parse-expr ts fail equal-j)])
+      (values `(SBind ,(hull mode-span (node-span value)) ,mode
+                      ,name-node ,annotation ,value)
+              value-j))))
+
+(define (parse-ty ts fail i)
+  (cond
+    [(eq? (kind-at ts i) 'ident)
+     (values `(TName ,(span-at ts i) ,(value-at ts i)) (add1 i))]
+    [(punct? ts i '|{|) (parse-type-record ts fail i)]
+    [(kw? ts i 'fn) (parse-function-type ts fail i)]
+    [(eof? ts i) (fail 'surface-unexpected-eof (span-at ts i))]
+    [else (fail 'surface-unexpected-token (span-at ts i))]))
+
+(define (parse-type-record ts fail i)
+  (define open (span-at ts i))
+  (define j0 (skip-nl ts (add1 i)))
+  (if (punct? ts j0 '|}|)
+      (values `(TRec ,(hull open (span-at ts j0)) ()) (add1 j0))
+      (let loop ([j j0] [fields '()])
+        (let*-values ([(label label-span label-j) (expect-ident ts fail j)]
+                     [(colon colon-j) (expect-punct ts fail label-j '|:|)]
+                     [(ty ty-j) (parse-ty ts fail colon-j)])
+          (define field
+            `(TField ,(hull label-span (node-span ty))
+                     (SLabel ,label-span ,label) ,ty))
+          (define next (skip-nl ts ty-j))
+          (cond
+            [(punct? ts next '|}|)
+             (values `(TRec ,(hull open (span-at ts next))
+                            ,(reverse (cons field fields)))
+                     (add1 next))]
+            [(punct? ts next '|,|)
+             (define after (skip-nl ts (add1 next)))
+             (if (punct? ts after '|}|)
+                 (values `(TRec ,(hull open (span-at ts after))
+                                ,(reverse (cons field fields)))
+                         (add1 after))
+                 (loop after (cons field fields)))]
+            [(> next ty-j) (loop next (cons field fields))]
+            [else (fail-at ts fail next)])))))
+
+(define (parse-function-type ts fail i)
+  (define start (span-at ts i))
+  (let*-values ([(open open-j) (expect-punct ts fail (add1 i) '|(|)]
+                [(types types-j) (parse-types ts fail open-j)]
+                [(close close-j) (expect-punct ts fail types-j '|)|)]
+                [(result result-j) (parse-ty ts fail close-j)])
+    (values `(TFn ,(hull start (node-span result)) ,types ,result) result-j)))
+
+(define (parse-params ts fail i)
+  (if (punct? ts i '|)|)
+      (values '() i)
+      (let loop ([j i] [params '()])
+        (let*-values ([(name name-span name-j) (expect-ident ts fail j)]
+                     [(colon colon-j) (expect-punct ts fail name-j '|:|)]
+                     [(ty ty-j) (parse-ty ts fail colon-j)])
+          (define param
+            `(SParam ,(hull name-span (node-span ty))
+                     (SName ,name-span ,name) ,ty))
+          (if (punct? ts ty-j '|,|)
+              (loop (add1 ty-j) (cons param params))
+              (values (reverse (cons param params)) ty-j))))))
+
+(define (parse-types ts fail i)
+  (if (punct? ts i '|)|)
+      (values '() i)
+      (let loop ([j i] [types '()])
+        (let-values ([(ty ty-j) (parse-ty ts fail j)])
+          (if (punct? ts ty-j '|,|)
+              (loop (add1 ty-j) (cons ty types))
+              (values (reverse (cons ty types)) ty-j))))))
+
+(define (parse-args ts fail i)
+  (if (punct? ts i '|)|)
+      (values '() i)
+      (let loop ([j i] [args '()])
+        (let-values ([(arg arg-j) (parse-expr ts fail j)])
+          (if (punct? ts arg-j '|,|)
+              (loop (add1 arg-j) (cons arg args))
+              (values (reverse (cons arg args)) arg-j))))))
--- model/redex/tests/parser-test.rkt
+++ model/redex/tests/parser-test.rkt
@@ -2,7 +2,10 @@
 
 (require rackunit
          redex/reduction-semantics
-         "../surface.rkt")
+         "../surface.rkt"
+         "../parser.rkt"
+         "../lexer.rkt"
+         "../diagnostic.rkt")
 
 (define s0 '(#:span src 0 1))
 
@@ -32,6 +35,101 @@
  (check-true
   (redex-match? Surface spitem
                 `(SBind ,s0 let (SName ,s0 x) #:none (SInt ,s0 1)))))
+
+(define (p str) (parse (lex/string 'src str)))
+
+(test-case
+ "整数だけの program"
+ (check-equal? (p "1")
+               '(SProgram (#:span src 0 1) () (SInt (#:span src 0 1) 1))))
+
+(test-case
+ "true と false は構成子であり literal ではない"
+ (check-equal? (p "true")
+               '(SProgram (#:span src 0 4) () (SBool (#:span src 0 4) true))))
+
+(test-case
+ "後置は適用と射影を左結合で積む"
+ (check-equal? (p "f(x).a")
+               '(SProgram
+                 (#:span src 0 6) ()
+                 (SProj (#:span src 0 6)
+                        (SApply (#:span src 0 4)
+                                (SVar (#:span src 0 1) f)
+                                ((SVar (#:span src 2 3) x)))
+                        (SLabel (#:span src 5 6) a)))))
+
+(test-case
+ "括弧で括った式は括弧の span を持たない"
+ ;; spec §5.3。pitem が無いので SProgram の span は末尾の expr の span と
+ ;; 一致する。括弧は span を持たないので、両端の ( と ) は含まれない。
+ (check-equal? (p "(x)")
+               '(SProgram (#:span src 1 2) () (SVar (#:span src 1 2) x))))
+
+(test-case
+ "空の波括弧は空の record である"
+ (check-equal? (p "{}")
+               '(SProgram (#:span src 0 2) () (SRec (#:span src 0 2) ()))))
+
+(test-case
+ "識別子と冒号が続けば record、そうでなければ block である"
+ (check-equal? (p "{ a: 1 }")
+               '(SProgram
+                 (#:span src 0 8) ()
+                 (SRec (#:span src 0 8)
+                       ((SField (#:span src 2 6)
+                                (SLabel (#:span src 2 3) a)
+                                (SInt (#:span src 5 6) 1))))))
+ (check-equal? (p "{ x }")
+               '(SProgram
+                 (#:span src 0 5) ()
+                 (SBlock (#:span src 0 5) () (SVar (#:span src 2 3) x)))))
+
+(test-case
+ "record の区切りは読点でも改行でもよい"
+ (check-equal? (length (third (fourth (p "{ a: 1\n b: 2 }")))) 2)
+ (check-equal? (length (third (fourth (p "{ a: 1, b: 2 }")))) 2))
+
+(test-case
+ "block の中の束縛は改行で区切る"
+ (check-equal? (p "{ let x = 1\n x }")
+               '(SProgram
+                 (#:span src 0 16) ()
+                 (SBlock (#:span src 0 16)
+                         ((SBind (#:span src 2 11) let
+                                 (SName (#:span src 6 7) x)
+                                 #:none
+                                 (SInt (#:span src 10 11) 1)))
+                         (SVar (#:span src 13 14) x)))))
+
+(test-case
+ "let mut は 2 つの kw から sbmode の mut へ正規化する"
+ (check-equal? (second (first (third (fourth (p "{ let mut x = 1\n x }")))))
+               '(#:span src 2 15))
+ (check-equal? (third (first (third (fourth (p "{ let mut x = 1\n x }")))))
+               'mut))
+
+(test-case
+ "無名関数は引数と返り値の型を取る"
+ (check-equal? (p "fn(a: Int) Int { a }")
+               '(SProgram
+                 (#:span src 0 20) ()
+                 (SFn (#:span src 0 20)
+                      ((SParam (#:span src 3 9)
+                               (SName (#:span src 3 4) a)
+                               (TName (#:span src 6 9) Int)))
+                      (TName (#:span src 11 14) Int)
+                      (SBlock (#:span src 15 20) ()
+                              (SVar (#:span src 17 18) a))))))
+
+(test-case
+ "出力は Surface の言語に合う"
+ (for ([src (in-list (list "1" "true" "f(x).a" "{}" "{ a: 1 }"
+                           "{ let x = 1\n x }"
+                           "fn(a: Int) Int { a }"
+                           "SInt" "TName" "none" "return"))])
+   (check-true (redex-match? Surface sprog (p src))
+               (format "~a の出力が Surface に合う" src))))
 
 (test-case
  "provide する述語が言語の判定と一致する"
